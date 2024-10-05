f86a30b98a29e6b2d486b32b682586c025446a7c
DESKTOP-QM6CI1B\EKALAND <alinazarii7394@gmail.com>
2024-10-05 21:08:26 +0330

    configure MapControllerRoute

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,22 +1,15 @@
-using System.Net.Mime;
-using System.Text;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", (HttpContext context) =>
-{
-    string html = @"<html><body><h1>Hello world</h1></body></html>";
+app.UseRouting();
 
-    WriteHtml(context, html);
-});
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller:Home}/{action=Index}/{id?} ");
 
 app.Run();
-
-
-void WriteHtml(HttpContext context, string html)
-{
-    context.Response.ContentType = MediaTypeNames.Text.Html;
-    context.Response.ContentLength = Encoding.UTF8.GetByteCount(html);
-    context.Response.WriteAsync(html);
-}
